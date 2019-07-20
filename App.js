af5aa75ba85f284662d754369216a04a6b84f9cb
davimattos <davimattosdev@gmail.com>
2019-07-20 16:47:48 -0300

    Animated header

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import {
   StyleSheet,
   Dimensions,
   ScrollView,
-  TouchableOpacity
+  TouchableOpacity,
+  Animated
 } from "react-native";
 
 import User from "./components/User";
@@ -18,6 +19,7 @@ const { width } = Dimensions.get("window");
 
 export default class App extends Component {
   state = {
+    scrollOffset: new Animated.Value(0),
     userSelected: null,
     userInfoVisible: false,
     users: [
@@ -60,7 +62,17 @@ export default class App extends Component {
           "https://images.unsplash.com/photo-1490633874781-1c63cc424610?auto=format&fit=crop&w=400&q=80",
         likes: 350,
         color: "#E75A63"
-      }
+      },
+      {
+        id: 1,
+        name: "Cláudio Orlandi",
+        description: "Head de programação!",
+        avatar: "https://avatars0.githubusercontent.com/u/2254731?s=460&v=4",
+        thumbnail:
+          "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=400&q=80",
+        likes: 622,
+        color: "#7159c1"
+      },
     ]
   };
 
@@ -77,7 +89,16 @@ export default class App extends Component {
 
   renderList = () => (
     <View style={styles.container}>
-      <ScrollView>
+      <ScrollView
+        scrollEventThrottle={16}
+        onScroll={Animated.event([
+          {
+            nativeEvent: {
+              contentOffset: { y: this.state.scrollOffset }
+            }
+          }
+        ])}
+      >
         {this.state.users.map(user => (
           <User
             key={user.id}
@@ -96,16 +117,36 @@ export default class App extends Component {
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
 
-        <View style={styles.header}>
+        <Animated.View 
+          style={[
+            styles.header,
+            {
+              height: this.state.scrollOffset.interpolate({
+                inputRange: [0, 140],
+                outputRange: [200, 70],
+                extrapolate: 'clamp'
+              })
+            }
+          ]}>
           <Image
             style={styles.headerImage}
             source={userSelected ? { uri: userSelected.thumbnail } : null}
           />
 
-          <Text style={styles.headerText}>
+          <Animated.Text 
+            style={[
+              styles.headerText,
+              {
+                fontSize: this.state.scrollOffset.interpolate({
+                  inputRange: [120, 140],
+                  outputRange: [24, 16],
+                  extrapolate: 'clamp'
+                })
+              }
+            ]}>
             {userSelected ? userSelected.name : "GoNative"}
-          </Text>
-        </View>
+          </Animated.Text>
+        </Animated.View>
         {this.state.userInfoVisible ? this.renderDetail() : this.renderList()}
       </View>
     );
@@ -121,7 +162,6 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === "ios" ? 40 : 20,
     paddingHorizontal: 15,
     backgroundColor: "#2E93E5",
-    height: 200
   },
 
   headerImage: {
@@ -129,7 +169,6 @@ const styles = StyleSheet.create({
   },
 
   headerText: {
-    fontSize: 24,
     fontWeight: "900",
     color: "#FFF",
     backgroundColor: "transparent",
